e3c3e2076d2ef0a41f13d8f832ed6ccb86f08661
李楠 <linanb@yonyou.com>
2017-02-20 23:15:44 +0800

    test
    Signed-off-by: 李楠 <linanb@yonyou.com>

---
+++ bike-manager-web/src/test/java/com/package-info.java
@@ -0,0 +1,15 @@
+/**   
+ * @Title: package-info.java 
+ * @Package com 
+ * @Description: TODO
+ * @author linan linanb@yonyou.com  
+ * @date 2017年2月20日 下午11:15:12 
+ */
+/** 
+ * @ClassName: package-info 
+ * @Description: TODO
+ * @author linan linanb@yonyou.com 
+ * @date 2017年2月20日 下午11:15:12 
+ *  
+ */
+package com;
