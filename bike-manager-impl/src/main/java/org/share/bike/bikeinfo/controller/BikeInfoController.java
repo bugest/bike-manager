7c9e8aa8f80087cd6b7b4c0885fdce01dd8d11f6
李楠 <linanb@yonyou.com>
2017-02-21 18:51:38 +0800

    TEST
    Signed-off-by: 李楠 <linanb@yonyou.com>

--- bike-manager-impl/src/main/java/org/share/bike/bikeinfo/controller/BikeInfoController.java
+++ bike-manager-impl/src/main/java/org/share/bike/bikeinfo/controller/BikeInfoController.java
@@ -5,8 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("first")
 public class BikeInfoController {
 	@Autowired
 	private IBikeInfoService bikeInfoService;
@@ -17,5 +20,11 @@ public class BikeInfoController {
 		logger.debug("DEBUG TEST 这个地方输出DEBUG级别的日志");  
         logger.info("INFO test 这个地方输出INFO级别的日志");  
         logger.error("ERROR test 这个地方输出ERROR级别的日志");  
+	}
+	@RequestMapping("test")
+	@ResponseBody
+	public String test(){
+		return "hello";
+		
 	}
 }
